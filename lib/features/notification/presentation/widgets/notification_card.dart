2615623a53a2f26396f271f985e6d29d593a4b07
yassmeen <yassmeensamy1@gmail.com>
2026-06-09 17:18:31 +0300

    change notification card

--- lib/features/notification/presentation/widgets/notification_card.dart
+++ lib/features/notification/presentation/widgets/notification_card.dart
@@ -28,7 +28,7 @@ class NotificationCard extends StatelessWidget {
       margin: const EdgeInsets.symmetric(horizontal: 4, vertical: 6),
       padding: const EdgeInsets.fromLTRB(14, 14, 14, 14),
       decoration: BoxDecoration(
-        color: AppColors.white.withValues(alpha: 0.55),
+        color: colors.cardSurface,
         borderRadius: BorderRadius.circular(ZaadRadii.card),
         border: Border.all(
           color: colors.olive.withValues(alpha: 0.16),
